540466c5f445aa72f58213bae3a77feef3aff59e
Zong Jhe Wu <s25g5d4@gmail.com>
2024-07-31 23:47:21 +0800

    update app name

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ import "dotenv/config";
 export const config = {
   token: process.env.DISCORD_BOT_TOKEN,
   clientId: process.env.DISCORD_CLIENT_ID,
-  app: process.env.APP_NAME ?? "Wake-up-bot",
+  app: process.env.APP_NAME ?? "Wake-bot",
   logLevel: process.env.LOG_LEVEL ?? "info",
   channels: process.env.CHANNELS,
   keywords: process.env.KEYWORDS ?? "我婆",
